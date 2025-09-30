320f4d96a357d56e78ed5af512d8c4d0cda221d4
Soullinker47 <linkersoul77@gmail.com>
2025-09-30 15:33:14 +0800

    fix(ci): enable Kotlin 2.0 Compose plugin and stabilize Android CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -24,7 +25,7 @@ android {
   }
 
   buildFeatures { compose = true }
-  composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
+
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
