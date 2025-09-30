b455defe4b8f7eb4e4c64cda321b9f6484d961b6
Soullinker47 <linkersoul77@gmail.com>
2025-09-30 18:07:38 +0800

    fix(ci): enable Kotlin 2.0 Compose plugin and stabilize Android CI

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Kotlin 2.0 requires the Compose compiler Gradle plugin to be declared at the root.
+plugins {
+  id("com.android.application") version "8.6.0" apply false
+  id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+  id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+}
